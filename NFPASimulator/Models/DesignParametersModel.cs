a444c933f3b73cbb27409eacd59a39acb854f61e
cesarecaoduro <Cesare.Caoduro@gmail.com>
2019-10-15 15:00:15 +1100

    Initiated Design parameters MVVM

---
+++ NFPASimulator/Models/DesignParametersModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NFPASimulator.Models
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class DesignParametersModel
+    {
+        public string Name { get; set; }
+        public double PlatformTravelSpeed { get; set; }
+        public double ConcourseTravelSpeed { get; set; }
+        public double SpeedElevation { get; set; }
+        public double BottleNeckFlowCapacity { get; set; }
+        public double MultiLeafDoorsFlowCapacity { get; set; }
+        public int TurnstileFlowCapacity { get; set; }
+        public double SingleLeafDoorsFlowCapacity { get; set; }
+        public double GatesFlowCapacity { get; set; }
+        public double StairsFlowCapacity { get; set; }
+        public double EscalatorFlowCapacity { get; set; }
+    }
+}
+
---
+++ NFPASimulator/ViewModels/DesignParametersViewModel.cs
@@ -0,0 +1,28 @@
+﻿using NFPASimulator.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NFPASimulator.ViewModels
+{
+    class DesignParametersViewModel : INotifyPropertyChanged
+    {
+        private DesignParametersModel DesignParameters;
+
+        public DesignParametersViewModel()
+        {
+            DesignParameters = new DesignParametersModel();
+        }
+
+        public string name
+        {
+            get => DesignParameters.Name;
+            set => DesignParameters.Name = value;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
