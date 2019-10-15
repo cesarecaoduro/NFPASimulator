477161863d7f4564f5f465ddaad89f47a6e92adb
cesarecaoduro <Cesare.Caoduro@gmail.com>
2019-10-15 15:14:58 +1100

    Initiated DesignParameters reader from JSON

--- NFPASimulator/Utilities/Utilities.cs
+++ NFPASimulator/Utilities/Utilities.cs
@@ -4,6 +4,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NFPASimulator.Models;
+using System.IO;
+using Newtonsoft.Json;
 
 namespace NFPASimulator.Utilities
 {
@@ -18,6 +21,18 @@ namespace NFPASimulator.Utilities
                 System.Windows.Media.Imaging.BitmapSizeOptions.FromEmptyOptions());
 
             return bmSource;
+        }
+
+        internal static DesignParametersModel[] DesignParametersFromJSON(string path)
+        {
+            DesignParametersModel[] designParameters;
+            using (StreamReader file = File.OpenText(path))
+            {
+                var jsonData = file.ReadToEndAsync().Result;
+                designParameters = JsonConvert.DeserializeObject<DesignParametersModel[]>(jsonData);
+
+            }
+            return designParameters;
         }
     }
 }
